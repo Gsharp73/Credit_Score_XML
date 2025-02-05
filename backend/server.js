997e85f95b32a9d144928c442c350c3c6725bda2
Gsharp73 <tripathidhyanendra2004@gmail.com>
2025-02-06 00:21:50 +0530

    vercel config

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+const uploadRoutes = require("./routes/uploadRoute");
+const reportRoutes = require("./routes/reportRoute");
+const deleteRoutes = require("./routes/deleteRoute");
+const historyRoutes = require("./routes/historyRoute")
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(fileUpload());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("MongoDB Connection Error: ", err));
+
+app.use("/upload", uploadRoutes);
+app.use("/reports", reportRoutes);
+app.use("/delete", deleteRoutes);
+app.use("/history", historyRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
